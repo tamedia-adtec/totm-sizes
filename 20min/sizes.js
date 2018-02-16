6c10724539953a06a3b743d359136833fb3e337f
Jubs <julia.barbosa@tamedia.ch>
2018-02-16 14:01:31 +0100

    added the complementary sizes on 20min and removing the non standard sizes from the default list

--- 20min/sizes.js
+++ 20min/sizes.js
@@ -1,15 +1,61 @@
 export default {
+
+  'inside-full-top': {
+      dfp: {
+          sizes : [[760, 190],[300, 250], [336, 280]]
+      },
+      appnexus: {
+          sizes : [300;250]
+  },
+
+  'inside-full': {
+      dfp: {
+          sizes : [[320,416],[336,280],[760,420],[994,550]]
+      },
+      appnexus: {
+          sizes : [320,416]
+  },
+
+
+  'gallery': {
+      dfp: {
+          sizes : [[468,400]]
+      },
+      appnexus: {
+          sizes : [[468,400]]
+      }
+  },
+
+  'inside-half': {
+      dfp: {
+          sizes : [[468,400]]
+      },
+      appnexus: {
+          sizes : [[468,400]]
+      }
+  },
+
+
+
     'outside-right': {
         dfp: {
-            sizes : [[600,1200]]
+            sizes : [[500,1000]]
         },
         appnexus: {
-            sizes : [[600,1200]]
+            sizes : [[500,1000]]
         }
     },
+
+
+    'outside-left': {
+      dfp: {
+          sizes : [160,600]]
+      },
+    }
+
     'content-ad': {
         forcedSizes : {
-            all : [[1, 1], [278, 40], [300, 250]]
+            all : []
         }
     }
 }
