95a491101acb9ad2f1fa9f7d3f9dda2533c3fe05
Jubs <julia.barbosa@tamedia.ch>
2018-03-08 16:27:30 +0100

    adding tutti_react

---
+++ tutti_react/sizes.js
@@ -0,0 +1,38 @@
+module.exports = {
+
+  'inside-full-top': {
+    blockedSizes: {
+        dfp: [[728,90]],
+        appnexus: [[728,90]]
+    }
+  },
+
+
+    'inside-full': {
+        sizes : {
+            dfp: [[278, 70], [278, 40]],
+            appnexus : []
+        }
+    },
+
+    'content-ad': {
+        sizes: {
+            dfp: [[278, 40]],
+            appnexus: []
+        }
+    },
+
+    'inside-quarter': {
+        sizes: {
+            dfp: [[278, 70], [300, 30],[300, 150]],
+            appnexus: [[278, 70], [300, 30],[300, 150]]
+        }
+    },
+
+    'outside-right': {
+        sizes: {
+            dfp: [[500, 1000]],
+            appnexus: [[500, 1000]]
+        }
+    }
+};
