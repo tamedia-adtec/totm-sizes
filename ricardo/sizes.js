b897d5acb85431137def08dfdd12c346448e6a66
Barbosa Julia <akxu@mediait.ch>
2018-04-30 16:54:15 +0200

    Ricardo now wants to run sitebars

--- ricardo/sizes.js
+++ ricardo/sizes.js
@@ -1,10 +1,10 @@
 module.exports = {
     'outside-right': {
-        blockedSizes: {
-            dfp: [[500, 1000]],
-            appnexus: [[500, 1000]]
-        }
-    },
+      sizes: {
+          dfp: [[500, 1000]],
+          appnexus: [[500, 1000]]
+      }
+  },
     // remove size ADT-658
     'content-ad': {
 
