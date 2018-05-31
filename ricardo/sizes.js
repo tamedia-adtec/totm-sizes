34ad32ae47f1cff8fbc7af518eba4392f946173a
Rapha84 <raphaela.luchsinger@tamedia.ch>
2018-05-31 14:56:42 +0200

    adding 500x1000 to ricardo DFP

--- ricardo/sizes.js
+++ ricardo/sizes.js
@@ -1,16 +1,26 @@
 module.exports = {
     'outside-right': {
       blockedSizes: {
-           dfp: [[500, 1000]],
            appnexus: [[500, 1000]]
        }
    },
+
+   //add 500x1000 to test sitebars DASB-1194
+   'outside-right': {
+    sizes: {
+         dfp: [[500, 1000]]
+     }
+ },
+
     // remove size ADT-658
     'content-ad': {
 
         sizes : {
             dfp : [[1,1]]
         },
+
+
+
         blockedSizes: {
             dfp: [[300, 250]],
             appnexus: [[300, 250]]
