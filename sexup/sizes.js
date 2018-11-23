b81352b32caa86ab6cc65ee8445ad46c0dcec896
tmsimba <31691182+tmsimba@users.noreply.github.com>
2018-11-23 10:24:47 +0100

    Update sizes.js

--- sexup/sizes.js
+++ sexup/sizes.js
@@ -1,7 +1,14 @@
 module.exports = {
     'inside-full-top': {
         sizes: {
-            dfp: [[300, 250]],
+            dfp: [[300, 250], [320, 416]],
+            appnexus: []
+        }
+    },
+
+    'inside-full-pos1': {
+        sizes: {
+            dfp: [[340, 170], [320, 416]],
             appnexus: []
         }
     },
@@ -13,33 +20,26 @@ module.exports = {
         }
     },
 
-      'inside-full-top': {
-        sizes: {
-            dfp: [[300, 250]],
-            appnexus: []
-        }
-    },
-
-       'inside-half-pos': {
+    'inside-half-pos': {
         sizes: {
             dfp: [[160, 100]],
             appnexus: []
         }
     },
 
-       'inside-quarter-top': {
+    'inside-quarter-top': {
         sizes: {
             dfp: [[300, 250]],
             appnexus: []
         }
     },
 
-           'outside-right-pos': {
+    'outside-right-pos': {
         sizes: {
             dfp: [[160, 100], [340, 170]],
             appnexus: []
         }
     }
- 
+
 
 };
