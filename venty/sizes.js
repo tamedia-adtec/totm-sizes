23f00c72464d96ff0116308be614b662c863b40e
tmsimba <31691182+tmsimba@users.noreply.github.com>
2019-02-18 15:14:53 +0100

    Adding 300x600

--- venty/sizes.js
+++ venty/sizes.js
@@ -1,7 +1,7 @@
 module.exports = {
     'content-ad': {
         sizes: {
-            dfp: [[320, 416], [640, 400]],
+            dfp: [[320, 416], [640, 400], [300, 600]],
             appnexus: [[320, 416]]
         }
     }
