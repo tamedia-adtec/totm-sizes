5ebe3ee2b15c9e795f1a6b362fdfd63e2dc53d46
tmsimba <31691182+tmsimba@users.noreply.github.com>
2019-05-28 13:24:28 +0200

    ADT-2011: neue size inside-full-pos1

--- starticket/sizes.js
+++ starticket/sizes.js
@@ -1,4 +1,10 @@
 module.exports = {
+    'inside-full-pos1': {
+        sizes: {
+            dfp: [[320, 416]],
+            appnexus: []
+        }
+    },
     'outside-right': {
         blockedSizes: {
             dfp: [[500, 1000]],
