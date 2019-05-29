1f56c72435542ed7a349a29bb5b0911a82a5d1fa
tmsimba <31691182+tmsimba@users.noreply.github.com>
2019-05-29 09:27:48 +0200

    DASB-1518: changes sebastien

--- lessentiel/sizes.js
+++ lessentiel/sizes.js
@@ -1,36 +1,42 @@
 module.exports = {
+    'overlay': {
+        sizes: {
+            dfp: [[1, 1], [320, 480], [480, 320], [768, 1024], [1024, 768], [1239, 800], [1920, 1080]],
+            appnexus: []
+        }
+    },
     'inside-full-top': {
         sizes: {
-            dfp: [[320,150]],
-            appnexus: [[320,150]]
+            dfp: [[320, 150]],
+            appnexus: [[320, 150]]
         }
     },
 
     'inside-full': {
         sizes: {
-            dfp: [[320, 416], [320,150]],
-            appnexus: [[320, 416],[320,150]]
+            dfp: [[320, 416], [320, 150]],
+            appnexus: [[320, 416], [320, 150]]
         }
     },
 
     'gallery': {
-        sizes : {
+        sizes: {
             dfp: [[468, 400]],
-            appnexus : [[468, 400]]
+            appnexus: [[468, 400]]
         }
     },
 
     'inside-half': {
-        sizes : {
-            dfp: [[468, 400],[470,470]],
-            appnexus : [[468, 400]]
+        sizes: {
+            dfp: [[468, 400], [470, 470]],
+            appnexus: [[468, 400]]
         }
     },
 
     'content-ad': {
-        sizes : {
-            dfp: [[1,1]],
-            appnexus : []
+        sizes: {
+            dfp: [[1, 1]],
+            appnexus: []
         }
     }
 };
