4627da17c90212cd555fbb9ac177fe14183e1141
tmsimba <31691182+tmsimba@users.noreply.github.com>
2019-06-03 15:14:06 +0200

    ADT-2019: neue size 300x600

--- annabelle/sizes.js
+++ annabelle/sizes.js
@@ -1,34 +1,34 @@
 module.exports = {
-      'inside-quarter': {
-          sizes : {
-              dfp: [[300,600]],
-              appnexus : [[300,600]]
-          }
+    'inside-quarter': {
+        sizes: {
+            dfp: [[300, 600]],
+            appnexus: [[300, 600]]
+        }
     },
 
     'inside-full-pos1': {
         sizes: {
-            dfp: [[300, 600]],
-            appnexus: []
+            dfp: [[300, 250], [320, 50], [320, 64], [320, 160], [336, 280], [728, 90], [970, 250], [994, 118], [994, 250], [300, 600]],
+            appnexus: [[994, 250], [970, 250], [728, 90], [300, 250], [320, 160], [320, 50], [320, 64]]
         }
     },
     'inside-full': {
-        sizes : {
-            dfp: [[320,416]],
-            appnexus : [[320,416]]
+        sizes: {
+            dfp: [[320, 416]],
+            appnexus: [[320, 416]]
         }
     },
 
     'content-ad': {
-        sizes : {
-            dfp: [[1,1]],
-            appnexus : []
+        sizes: {
+            dfp: [[1, 1]],
+            appnexus: []
         }
     },
     'outside-right': {
-        sizes : {
+        sizes: {
             dfp: [[500, 1000]],
-            appnexus : [[500, 1000]]
+            appnexus: [[500, 1000]]
         }
     }
- };
+};
