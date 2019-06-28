d83c775eac3c88f73e31121d50ebb16ba1e9850e
Torsten <Torsten.Dunkel@tamedia.ch>
2019-06-28 12:50:03 +0200

    paid-inside for tio inapp

--- tio_app/sizes.js
+++ tio_app/sizes.js
@@ -48,7 +48,12 @@ module.exports = {
             dfp: [[1,1]],
             appnexus : []
         }
+    },
+    'paid-inside':{
+        sizes : {
+            dfp: [[1,1]],
+            appnexus : []
+        }
     }
-    
 
 };
