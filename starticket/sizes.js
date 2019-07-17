aa268fa8c681cce44e6131074147dfba603a74e0
Torsten <Torsten.Dunkel@tamedia.ch>
2019-07-17 15:11:46 +0200

    using basic sizes for starticket

--- starticket/sizes.js
+++ starticket/sizes.js
@@ -1,10 +1,5 @@
 module.exports = {
-    'inside-full-pos1': {
-        sizes: {
-            dfp: [[320, 416], [300, 250], [320, 50], [320, 64], [320, 160], [336, 280], [828, 910]],
-            appnexus: []
-        }
-    },
+
     'outside-right': {
         blockedSizes: {
             dfp: [[500, 1000]],
