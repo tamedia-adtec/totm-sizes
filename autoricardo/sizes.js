ffc892f035c5cfd332debff06323b35a815d9b02
Torsten <Torsten.Dunkel@tamedia.ch>
2019-10-25 10:59:31 +0200

    added also to prebid

--- autoricardo/sizes.js
+++ autoricardo/sizes.js
@@ -27,7 +27,8 @@ module.exports = {
     },
     'outside-right': {
         sizes: {
-            dfp: [[500, 1000]]
+            dfp: [[500, 1000]],
+            appnexus: [[500, 1000]]
         }
     }
 };
