8cb3587d50ce4543b842c2bfca7355b04033abdf
laura-basler <laura.basler@digitaladservices.ch>
2020-01-09 14:06:06 +0100

    add size 994x250 for tablet

--- tio/sizes.js
+++ tio/sizes.js
@@ -2,7 +2,7 @@ module.exports = {
     // add extra sizes PP-664, ADT-395
     'inside-full-top': {
         sizes: {
-            dfp: [[720,145],[1024,122], [320,250], [300,250]],
+            dfp: [[720,145],[1024,122], [320,250], [300,250], [994,250]],
             appnexus: []
         },
 
