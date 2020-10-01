2c6a8150f6e2fa50d16f72eacb4c02fe89663079
Torsten <Torsten.Dunkel@tamedia.ch>
2020-10-01 12:26:55 +0200

    added some standard sizes to the immo satelites

--- home/sizes.js
+++ home/sizes.js
@@ -10,13 +10,18 @@ module.exports = {
 
     'inside-full': {
         sizes: {
-            dfp: [[468, 60]],
-            appnexus: []
+            dfp: [[640, 320],[994,550],[320, 416]],
+            appnexus: [[994,550],[320, 416]]
         },
 
         blockedSizes: {
             dfp: [[970, 250], [994, 118], [994, 250], [994, 500]],
             appnexus: [[970, 250], [994, 118], [994, 250], [994, 500]]
+        },
+        forcedSizes: {
+            all: [],
+            mobile: [[828, 910]],
+            desktop: [[960, 800]]
         }
 
     },
--- immostreet/sizes.js
+++ immostreet/sizes.js
@@ -19,8 +19,13 @@ module.exports = {
 
     'inside-full': {
         sizes: {
-            dfp: [[640, 320]],
-            appnexus: []
+            dfp: [[640, 320],[994,550],[320, 416]],
+            appnexus: [[994,550],[320, 416]]
         },
+        forcedSizes: {
+            all: [],
+            mobile: [[828, 910]],
+            desktop: [[960, 800]]
+        }
     }
 };
