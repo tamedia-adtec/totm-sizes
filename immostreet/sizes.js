2e2c0e69431273c7eb740ab4245dfb3f8df67bad
Torsten <Torsten.Dunkel@tamedia.ch>
2020-10-01 12:29:03 +0200

    removed forced poster sizes

--- immostreet/sizes.js
+++ immostreet/sizes.js
@@ -21,11 +21,6 @@ module.exports = {
         sizes: {
             dfp: [[640, 320],[994,550],[320, 416]],
             appnexus: [[994,550],[320, 416]]
-        },
-        forcedSizes: {
-            all: [],
-            mobile: [[828, 910]],
-            desktop: [[960, 800]]
         }
     }
 };
