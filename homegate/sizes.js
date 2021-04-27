8cbd951733830d4699e6d9b62324312617f7e421
silvan <silvan.leutenegger@tamedia.ch>
2021-04-27 15:49:16 +0200

    DASB-2362, DASB-2363 poster ad and sidebar

--- homegate/sizes.js
+++ homegate/sizes.js
@@ -52,6 +52,13 @@ module.exports = {
         }
     },
 
+    'outside-right': {
+        sizes: {
+            dfp: [[500, 1000]],
+            appnexus: [[500, 1000]]
+        }
+    },
+
     'inside-full-searchbox': {
         sizes: {
             dfp: [[1, 1]],
