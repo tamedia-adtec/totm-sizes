d8f904c3d54d31bcc8bfaefe98b13f78dd67e400
Torsten Dunkel <Torsten.Dunkel@goldbach.com>
2021-07-08 10:43:24 +0200

    DASB-2516: corrected typo

--- commercial_publishing/sizes.js
+++ commercial_publishing/sizes.js
@@ -7,11 +7,11 @@ module.exports = {
     },
 
     'inside-full-content': {
+        sizes: {
+            dfp: [[994, 500]],
+            appnexus: [[994, 500]]
+        },
         forcedSizes: {
-            sizes: {
-                dfp: [[994, 500]],
-                appnexus: [[994, 500]]
-            },
             all: [[320, 416], [300, 250]]
         }
     },
