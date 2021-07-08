e53a30bb1f4821d19d9f85e2caef779f47babce0
Torsten Dunkel <Torsten.Dunkel@goldbach.com>
2021-07-08 10:39:41 +0200

    DASB-2516: 994x500 for cp

--- commercial_publishing/sizes.js
+++ commercial_publishing/sizes.js
@@ -1,14 +1,18 @@
 module.exports = {
-    'paid-inside':{
-        sizes : {
-            dfp: [[1,1]],
-            appnexus : []
+    'paid-inside': {
+        sizes: {
+            dfp: [[1, 1]],
+            appnexus: []
         }
     },
 
     'inside-full-content': {
         forcedSizes: {
-            all: [[320, 416],[300, 250]]
+            sizes: {
+                dfp: [[994, 500]],
+                appnexus: [[994, 500]]
+            },
+            all: [[320, 416], [300, 250]]
         }
     },
 };
