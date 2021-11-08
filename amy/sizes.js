700178fba3a2da2bd3b4882dfaf18cd20bb3a383
Torsten Dunkel <Torsten.Dunkel@goldbach.com>
2021-11-08 14:09:14 +0100

    DASB-2677 added amy sizes

--- amy/sizes.js
+++ amy/sizes.js
@@ -2,11 +2,11 @@ module.exports = {
     'inside-full-top': {
         sizes: {
             dfp: [],
-            appnexus: [],
-            blockedSizes: {
-                dfp: [[994, 500]],
-                appnexus: [[994, 500]]
-            }
+            appnexus: []
+        },
+        blockedSizes: {
+            dfp: [[994, 500]],
+            appnexus: [[994, 500]]
         }
     },
 
