299d57c6f89d96c360ff0f9e3306cc0f2225b459
tmsimba <31691182+tmsimba@users.noreply.github.com>
2022-01-11 13:58:55 +0100

    meter-magazin config added

---
+++ meter-magazin/sizes.js
@@ -0,0 +1,41 @@
+module.exports = {
+    'inside-full-top': {
+        sizes: {
+            dfp: [],
+            appnexus: []
+        },
+        blockedSizes: {
+            dfp: [[994, 500]],
+            appnexus: [[994, 500]]
+        }
+    },
+
+    'inside-full': {
+        sizes: {
+            dfp: [],
+            appnexus: []
+        },
+        blockedSizes: {
+            dfp: [[960, 800], [828, 910]],
+            appnexus: [[960, 800], [828, 910]]
+        }
+    },
+
+    'inside-full-content': {
+        sizes: {
+            dfp: [[320,416]],
+            appnexus: [[320,416]]
+        },
+        blockedSizes: {
+            dfp: [[960, 800], [828, 910]],
+            appnexus: [[960, 800], [828, 910]]
+        }
+    },
+
+    'outside-right': {
+    sizes: {
+        dfp: [[500, 1000]],
+            appnexus: [[500, 1000]]
+    }
+},
+};
