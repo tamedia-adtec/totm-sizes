c283d9932c24293eda05ea8eba40813ef9b7bdf3
Torsten Dunkel <Torsten.Dunkel@goldbach.com>
2022-11-10 13:34:42 +0100

    added swiss sizes config

---
+++ swiss/sizes.js
@@ -0,0 +1,36 @@
+module.exports = {
+    'inside-full-top': {
+        sizes: {
+            dfp: [[300, 250], [320, 416]],
+            appnexus: []
+        }
+    },
+
+    'inside-full-pos1': {
+        sizes: {
+            dfp: [[320, 416]],
+            appnexus: []
+        }
+    },
+
+    'inside-full-pos': {
+        sizes: {
+            dfp: [],
+            appnexus: []
+        }
+    },
+
+    'inside-half-pos': {
+        sizes: {
+            dfp: [],
+            appnexus: []
+        }
+    },
+
+    'inside-quarter-top': {
+        sizes: {
+            dfp: [[300, 250]],
+            appnexus: []
+        }
+    }
+};
